85134f35a5338d11a84ff0163bd071c91507bd8f
Miguel Angel Echeverri Quiroz <miguel.quiroz@gig-ext.com>
2023-01-19 08:16:34 +0100

    scroll to top when the path changes

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Cart from "../pages/cart";
 import Home from "../pages/home";
 import Register from "../pages/register";
+import ScrollToTop from "./scrollTop";
 
 const Router = () => {
   return (
     <BrowserRouter>
+    <ScrollToTop/>
       <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
