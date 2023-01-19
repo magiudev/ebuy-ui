725acb21e1bb783db4ddd136d78bfd01971ecb2e
Miguel Angel Echeverri Quiroz <miguel.quiroz@gig-ext.com>
2023-01-19 08:08:07 +0100

    create product in cart component

---
+++ src/pages/cart/components/product/index.tsx
@@ -0,0 +1,44 @@
+import "./style.css"
+
+const ProductInCart = ({
+    id, 
+    name, 
+    color, 
+    size, 
+    price, 
+    image
+}: {
+ id: string | number;
+ name: string;
+ color: string;
+ price: number;
+ size: string | number;
+ image: string;
+}) => {
+
+  return (
+    <div className="product-cart__container">
+        <img 
+            className="product-cart__image"
+            src={image}
+        />
+        <div className="product-cart__info-container">
+            <p><b>Product: </b>{name}</p>
+            <p><b>ID: </b>{id}</p>
+            <div className="product-cart__color-container">
+                <p><b>Color: </b></p>
+                <div className="product-cart__product-color" style={{ backgroundColor: color}}></div>
+            </div>
+            <p><b>Size: </b>{size}</p>
+            <div className="product-cart__counter-container">
+                <button className="product-cart__quantity-button">-</button>
+                <input type="text" name="quantity" value="1" className="product-cart__quantity-input" />
+                <button className="product-cart__quantity-button">+</button>
+            </div>
+            <p className="product-cart__price">$ {price}</p>
+        </div>
+    </div>
+  )
+}
+
+export default ProductInCart;
